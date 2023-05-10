455869dcd1369ec4d1067df483606f9bde95b0eb
internship311 <techtosh588@gmail.com>
2023-05-10 23:55:35 +0530

    Maximum SubArrays Solution

---
+++ Arrays/MaximumSubArrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum = 0, maxsum = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < nums.length; i++) {
+            currsum = currsum + nums[i];
+            maxsum = Math.max(currsum, maxsum);
+            if(currsum < 0) {
+                currsum = 0;
+            }
+        }
+        
+        return maxsum;
+    }
+}
